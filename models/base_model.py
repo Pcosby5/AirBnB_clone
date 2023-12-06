04222252c26ea97f1148edd1eff847a308a67e57
Pcosby5 <pcosby50@gmail.com>
2023-12-06 10:57:41 +0000

    updated base_model

--- models/base_model.py
+++ models/base_model.py
@@ -10,12 +10,19 @@ class BaseModel:
 
     """Class from which other classes will inherit"""
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """creates an instance of all attributes"""
+        if len(kwargs) > 0:
+            for key, value in kwargs.items():
+                if key != "__class__":
+                    if key == "created_at" or key == "updated_at":
+                        value = datetime.fromisoformat(value)
+                    setattr(self, key, value)
+            return
 
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
-        self.updated_at = self.created_at
+        self.updated_at = datetime.now()
 
     def __str__(self):
         """Returns official string representation"""
