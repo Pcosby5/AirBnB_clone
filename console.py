eaca464442ce6c63ad9e78052a531c677dbb7cd7
Pcosby5 <pcosby50@gmail.com>
2023-12-07 18:28:18 +0000

    console.py

--- console.py
+++ console.py
@@ -54,6 +54,44 @@ class HBNBCommand(cmd.Cmd):
                 else:
                     print(storage.all()[obj_key])
 
+    def do_destroy(self, arg):
+        """Deletes an instance of a class name and id"""
+        if arg is None or arg == "":
+            print("** class name missing **")
+        else:
+            args = arg.split(' ')
+            if args[0] not in storage.classes():
+                print("** class doesn't exist **")
+            elif len(args) < 2:
+                print("** instance id missing **")
+            else:
+                obj_key = "{}.{}".format(args[0], args[1])
+                if obj_key not in storage.all():
+                    print("** no instance found **")
+                else:
+                    del storage.all()[obj_key]
+                    storage.save()
+
+    def do_all(self, arg):
+        """Prints all string representations of all instances"""
+        if arg is not "":
+            args = arg.split(' ')
+            if args[0] not in storage.classes():
+                print("** class doesn't exist **")
+            else:
+                string_rep = [str(obj) for obj_key, obj in storage.all().items()
+                        if type(obj).__name__ == args[0]]
+                print(string_rep)
+
+    def do_update(self, arg):
+        """Updates an instance based on the class name
+        and id by adding or updating attribute"""
+
+
+
+
+
+
 
 
 
