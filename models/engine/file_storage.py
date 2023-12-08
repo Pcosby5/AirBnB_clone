df3ff36b53858329e145f3e38c68aca699c9197b
Fred010 <fjbaafi18@gmail.com>
2023-12-08 08:37:33 -0800

    place attributes

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -72,5 +72,17 @@ class FileStorage:
                     "name": str},
                 "Amenity":
                 {"name": str},
+                "Place":
+                {"city_id": str,
+                    "user_id": str,
+                    "name": str,
+                    "description": str,
+                    "number_rooms": int,
+                    "number_bathrooms": int,
+                    "max_guest": int,
+                    "price_by_night": int,
+                    "latitude": float,
+                    "longitude": float,
+                    "amenity_ids": list},
         }
         return attributes
