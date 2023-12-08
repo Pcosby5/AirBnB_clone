081f3b37bdbf7b393d42229c40ac052020c5e568
Fred010 <fjbaafi18@gmail.com>
2023-12-08 04:01:19 -0800

    state sub-class

---
+++ models/state.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Defines the state sub-class"""
+from models.base_model import BaseModel
+
+
+class State(BaseModel):
+    """Defines sub-class inheriting from BaseModel class
+
+    Attributes:
+    name (string): name of state
+    """
+
+    name = " "
