eeef5c265ae601a5c0bc01901ca58e9891bd71ba
Fred010 <fjbaafi18@gmail.com>
2023-12-10 14:28:14 -0800

    Console 0.1 classes

--- console.py
+++ console.py
@@ -28,6 +28,41 @@ class HBNBCommand(cmd.Cmd):
         """Quit command to exit the program"""
         print("Quit command to exit the program")
 
+    def do_create(self, arg):
+        """Creates instance of BaseModel and saves to JSON file
+
+        Usage: create <class>
+        """
+        pass
+
+    def do_show(self, arg):
+        """Set outs string repr of an instance
+
+        Usage: Displays <class_name> <id>
+        """
+        pass
+
+    def do_destroy(self, arg):
+        """Deletes class instances based on class_name and id
+
+        Usage: Destroys <class_name> and <id>
+        """
+        pass
+
+    def do_all(self, arg):
+        """Prints string repr of all instances or specific classes
+
+        Usage: all[class_name]
+        """
+        pass
+
+    def do_update(self, arg):
+        """Updates instance by adding or updating attribute
+
+        Usage: update <class_name> <id> <attribute_name> <attribute value>
+        """
+        pass
+
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
