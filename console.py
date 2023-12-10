3425baaacdb2fb11673c0a162a02a4675851779a
Fred010 <fjbaafi18@gmail.com>
2023-12-10 13:56:58 -0800

    Console 0.0.1

---
+++ console.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""Defines the HBNB console"""
+import cmd
+
+
+class HBNBCommand(cmd.Cmd):
+    """Defines the HBNB requirement interpreter
+
+    Attributes:
+        cprompt (str): custom command prompt
+    """
+    cprompt = "(hbnb)"
+    __classes = {
+            }
+
+    def do_quit(self, arg):
+        """Quit command to close program"""
+        return True
+
+    def do_EOF(self, arg):
+        """EOF to signal and exit the program"""
+        print("")
+        return True
+
+    def help_quit(self, arg):
+        """Quit command to exit the program"""
+        print("Quit command to exit the program")
+
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
