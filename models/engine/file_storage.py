2207e7d4d92761812b9b2ce3b07fb353fd3172d8
Pcosby5 <pcosby50@gmail.com>
2023-12-11 02:51:03 +0000

    file_storage.py

---
+++ models/engine/file_storage.py
@@ -0,0 +1,34 @@
+from models.base_model import BaseModel
+import json
+from os.path import exists
+
+
+class FileStorage:
+    __file_path = "file.json"
+    __objects = {}
+
+    def all(self):
+        return self.__objects
+
+    def new(self, obj):
+        key = "{}.{}".format(obj.__class__.__name__, obj.id)
+        self.__objects[key] = obj
+
+    def save(self):
+        serialized_obj = {
+            key: obj.to_dict() for key, obj in self.__objects.items()}
+        with open(self.__file_path, 'w') as file:
+            json.dump(serialized_obj, file)
+
+    def reload(self):
+        try:
+            if exists(self.__file_path):
+                with open(self.__file_path, 'r') as file:
+                    data = json.load(file)
+                    for key, value in data.items():
+                        class_name, obj_id = key.split('.')
+                        class_module = globals().get(class_name)
+                        if class_module:
+                            self.__objects[key] = class_module(**value)
+        except FileNotFoundError:
+            pass
