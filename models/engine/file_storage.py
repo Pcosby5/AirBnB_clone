c95950946855688380f807b3dd6e65a376c92b18
Fred010 <fjbaafi18@gmail.com>
2023-12-12 14:47:44 -0800

    changes to file storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,15 +1,18 @@
 #!/usr/bin/python3
 """Defines FileStorage class"""
-from models.base_model import BaseModel
 import json
+from models.base_model import BaseModel
 from models.user import User
 from models.place import Place
 from models.state import State
 from models.city import City
 from models.amenity import Amenity
 from models.review import Review
+<<<<<<< Updated upstream
 from os.path import exists
 import datetime
+=======
+>>>>>>> Stashed changes
 
 
 class FileStorage:
